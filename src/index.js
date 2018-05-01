5400ff20c65b1a1554c5c84e1b161f41163ef7d7
chawes <hawes.conor@gmail.com>
2018-05-01 15:16:49 -0500

    Added marker

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello from JavaScript");
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1IjoiY2hhd2VzIiwiYSI6ImNqZ28zaDg3NzBvdW8ycXIxbTd1cGgxb2QifQ.qqh3iNQQkCywkfa8ozkMmg";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-87.639, 41.8954], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 15, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+const markerElement = document.createElement('div');
+markerElement.style.width = "15px";
+markerElement.style.height = "15px";
+markerElement.style.backgroundColor = "red";
+
+new mapboxgl.Marker(markerElement).setLngLat([-87.639, 41.8954]).addTo(map);
